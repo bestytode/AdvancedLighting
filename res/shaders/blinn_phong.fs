c46d975f887fd3989b8f6b223fa7ed3f71200599
余振寰 <1932878552@qq.com>
2023-08-07 16:50:17 +0800

    1

--- res/shaders/blinn_phong.fs
+++ res/shaders/blinn_phong.fs
@@ -20,18 +20,20 @@ uniform Light light;
 
 void main()
 {
+	// Basic attributes
+	vec3 lightDir = normalize(lightPos - FragPos);
+	vec3 normal = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
+
 	// Ambient
 	vec3 ambient = light.ambient * texture(floorTexture, TexCoords).rgb;
 
 	// Diffuse
-	vec3 lightDir = normalize(lightPos - FragPos);
-	vec3 normal = normalize(Normal);
 	float diff = max(dot(lightDir, normal), 0.0f);
 	vec3 diffuse = light.diffuse * diff * texture(floorTexture, TexCoords).rgb;
 
 	// Specular
 	float spec = 0.0f;
-	vec3 viewDir = normalize(viewPos - FragPos);
 	if(blinn) {
 		vec3 halfWayVector = normalize(lightDir + viewDir);
 		spec = pow(max(dot(halfWayVector, normal), 1.0f), 32.0f);
