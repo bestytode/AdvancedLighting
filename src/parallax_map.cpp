94d1f7c33dac4fafb5bb5c1ef80cbc20e93c4c9a
余振寰 <1932878552@qq.com>
2023-09-11 15:15:40 +0800

    1

---
+++ src/parallax_map.cpp
@@ -0,0 +1,284 @@
+#include <iostream>
+#include <stdexcept>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "camera.h"
+#include "model.h"
+#include "shader.h"
+
+// Function declarations
+void framebuffer_size_callback(GLFWwindow* window, int SCR_WIDTH, int SCR_HEIGHT);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void ProcessInput(GLFWwindow* window);
+unsigned int LoadTexture(const std::string& path);
+void RenderQuad();
+
+// Scene settings
+constexpr int SCR_WIDTH = 800;
+constexpr int SCR_HEIGHT = 600;
+constexpr float height_scale = 0.1f;
+
+// Camera settings
+Camera camera(0.0f, 0.0f, 5.0f);
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+bool firstMouse = true;
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main()
+{
+	// glfw & glew configs
+	GLFWwindow* window = nullptr;
+	try {
+		if (!glfwInit())
+			throw std::runtime_error("failed to init glfw");
+		window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "hnzz", nullptr, nullptr);
+		if (!window)
+			throw std::runtime_error("failed to create window");
+
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		glfwSetCursorPosCallback(window, mouse_callback);
+		glfwSetScrollCallback(window, scroll_callback);
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		if (glewInit() != GLEW_OK)
+			throw std::runtime_error("failed to init glew");
+
+		// OpenGL global status settings here
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+
+	// Build & compile shader(s)
+	Shader shader("res/shaders/parallax_map.vs", "res/shaders/parallax_map,fs");
+
+	// Load texture(s)
+	unsigned int diffuseMap = LoadTexture("...");
+	unsigned int normalMap = LoadTexture("...");
+	unsigned int heightMap = LoadTexture("...");
+
+	shader.Bind();
+	shader.SetInt("diffuseMap", 0);
+	shader.SetInt("normalMap", 1);
+	shader.SetInt("heightMap", 2);
+
+	// lighting info
+	glm::vec3 lightPos(0.5f, 1.0f, 0.3f);
+
+	int counter = 0;
+	const int maxPrints = 50;
+	while (!glfwWindowShouldClose(window)) {
+		// Per-frame logic
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// fps printing
+		if (counter < maxPrints) {
+			std::cout << "fps: " << 1.0f / deltaTime << "\n";
+			counter++;
+		}
+
+		// Render
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Process input
+		ProcessInput(window);
+
+		// render parallax-mapped quad
+		glm::mat4 projection = glm::perspective(glm::radians(camera.fov), (float)SCR_HEIGHT / (float)SCR_WIDTH, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+
+		shader.SetMat4("projection", projection);
+		shader.SetMat4("view", view);
+		shader.SetMat4("model", model);
+		
+		shader.SetVec3("viewPos", camera.position);
+		shader.SetVec3("lightPos", lightPos);
+		shader.SetFloat("height_scale", height_scale);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, normalMap);
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, heightMap);
+
+		RenderQuad();
+
+		// Render light source
+		// todo
+		// 
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int SCR_WIDTH, int SCR_HEIGHT)
+{
+	// make sure the viewport matches the new window dimensions; note that SCR_WIDTH and 
+	// SCR_HEIGHT will be significantly larger than specified on retina displays.
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// Utility function for loading a 2D texture from file
+unsigned int LoadTexture(const std::string& path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+	if (data) {
+		GLenum format = GL_RGBA;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else {
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
+
+
+void RenderQuad()
+{
+	// Define vertices for a 10x10 quad
+	// Note: This example regenerates vertices and indices every time for simplicity.
+	// For performance-critical applications, consider optimizing this.
+	float quadVertices[] = {
+		// positions        // normals          // texture coordinates  // tangents           // bitangents
+		0.0f,  10.0f, 0.0f,  0.0f, 0.0f, 1.0f,    0.0f, 1.0f,            1.0f, 0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
+		10.0f, 10.0f, 0.0f,  0.0f, 0.0f, 1.0f,    1.0f, 1.0f,            1.0f, 0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
+		10.0f, 0.0f,  0.0f,  0.0f, 0.0f, 1.0f,    1.0f, 0.0f,            1.0f, 0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
+		0.0f,  0.0f,  0.0f,  0.0f, 0.0f, 1.0f,    0.0f, 0.0f,            1.0f, 0.0f, 0.0f,     0.0f, 1.0f, 0.0f
+	};
+
+	// CCW order
+	unsigned int quadIndices[] = {
+		2, 1, 0,
+		0, 3, 2
+	};
+
+	unsigned int VAO, VBO, EBO;
+
+	// Generate and configure buffer objects
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Texture coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// Tangent attribute
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+	glEnableVertexAttribArray(3);
+
+	// Bitangent attribute
+	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+	glEnableVertexAttribArray(4);
+
+	// Render the quad
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	// Delete the VAO and VBOs
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
