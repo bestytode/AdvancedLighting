6b4eb956ee0594f8cab7d818ac35f077affb5a41
余振寰 <1932878552@qq.com>
2023-09-24 13:51:55 +0100

    1

---
+++ src/deferred_shading.cpp
@@ -0,0 +1,270 @@
+#include <iostream>
+#include <stdexcept>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "camera.h"
+#include "shader.h"
+#include "geometry_renderers.h"
+#include "model.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+// Function declarations (callback functions)
+void framebuffer_size_callback(GLFWwindow* window, int SCR_WIDTH, int SCR_HEIGHT);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void ProcessInput(GLFWwindow* window);
+unsigned int LoadTexture(const std::string& path);
+
+// Scene settings
+constexpr int SCR_WIDTH = 1920;
+constexpr int SCR_HEIGHT = 1080;
+
+// Camera configs
+Camera camera(0.0f, 0.0f, 5.0f);
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+bool mouseButtonPressed = true;
+
+// Timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main()
+{
+	// glfw & glew configs
+	GLFWwindow* window = nullptr;
+	try {
+		if (!glfwInit())
+			throw std::runtime_error("failed to init glfw");
+		window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "hnzz", nullptr, nullptr);
+		if (!window)
+			throw std::runtime_error("failed to create window");
+
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		glfwSetCursorPosCallback(window, mouse_callback);
+		glfwSetScrollCallback(window, scroll_callback);
+
+		if (glewInit() != GLEW_OK)
+			throw std::runtime_error("failed to init glew");
+
+		// OpenGL global status settings here
+		glEnable(GL_DEPTH_TEST);
+		//glEnable(GL_CULL_FACE);
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+
+	// ImGui Initialization
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330 core");
+
+	// Build & compile shader(s)
+	Shader shaderGeometryPass("8.1.g_buffer.vs", "8.1.g_buffer.fs");
+	Shader shaderLightingPass("8.1.deferred_shading.vs", "8.1.deferred_shading.fs");
+	Shader shaderLightBox("8.1.deferred_light_box.vs", "8.1.deferred_light_box.fs");
+
+	// Load model(s)
+	Model backpack(".../backpack.obj");
+	std::vector<glm::vec3> objectPositions;
+	objectPositions.push_back(glm::vec3(-3.0, -0.5, -3.0));
+	objectPositions.push_back(glm::vec3(0.0, -0.5, -3.0));
+	objectPositions.push_back(glm::vec3(3.0, -0.5, -3.0));
+	objectPositions.push_back(glm::vec3(-3.0, -0.5, 0.0));
+	objectPositions.push_back(glm::vec3(0.0, -0.5, 0.0));
+	objectPositions.push_back(glm::vec3(3.0, -0.5, 0.0));
+	objectPositions.push_back(glm::vec3(-3.0, -0.5, 3.0));
+	objectPositions.push_back(glm::vec3(0.0, -0.5, 3.0));
+	objectPositions.push_back(glm::vec3(3.0, -0.5, 3.0));
+
+	// configure g-buffer framebuffer
+	// ------------------------------
+	// TODO
+	
+	// Lighting info
+	const unsigned int NR_LIGHTS = 32;
+	std::vector<glm::vec3> lightPositions;
+	std::vector<glm::vec3> lightColors;
+	srand(13);
+	for (size_t i = 0; i < NR_LIGHTS; i++) {
+		// calculate slightly random offsets
+		float xPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 3.0);
+		float yPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 4.0);
+		float zPos = static_cast<float>(((rand() % 100) / 100.0) * 6.0 - 3.0);
+		lightPositions.push_back(glm::vec3(xPos, yPos, zPos));
+		// also calculate random color
+		float rColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+		float gColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+		float bColor = static_cast<float>(((rand() % 100) / 200.0f) + 0.5); // between 0.5 and 1.0
+		lightColors.push_back(glm::vec3(rColor, gColor, bColor));
+	}
+
+	// shader configs
+	shaderLightingPass.Bind();
+	shaderLightingPass.SetInt("gPosition", 0);
+	shaderLightingPass.SetInt("gNormal", 1);
+	shaderLightingPass.SetInt("gAlbedoSpec", 2);
+
+	// Imgui settings
+	bool firstTime = false;
+	while (!glfwWindowShouldClose(window)) {
+		// Per-frame logic
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// Render
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Process input
+		ProcessInput(window);
+
+		// 1. geometry pass: render scene's geometry/color data into gbuffer
+        // -----------------------------------------------------------------
+		// 2. lighting pass: calculate lighting by iterating over a screen filled quad pixel-by-pixel using the gbuffer's content.
+		// -----------------------------------------------------------------------------------------------------------------------
+		// 2.5. copy content of geometry's depth buffer to default framebuffer's depth buffer
+		// ----------------------------------------------------------------------------------
+	    // 3. render lights on top of scene
+		// --------------------------------
+		// TODO
+		
+
+		// ImGui code here
+		// ---------------
+		// ImGui Frame Start
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// ImGui GUI code
+		if (firstTime) {
+			ImGui::SetNextWindowSize(ImVec2(500, 400));
+			ImGui::SetNextWindowPos(ImVec2(50, 50));
+			firstTime = false;
+		}
+		ImGui::Begin("hnzz");
+		ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+		ImGui::End();
+
+		// ImGui Rendering
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Optional: release all the resources of OpenGL (VAO, VBO, etc.)
+	glfwTerminate();
+
+	// ImGui Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int SCR_WIDTH, int SCR_HEIGHT)
+{
+	// make sure the viewport matches the new window dimensions; note that SCR_WIDTH and 
+	// SCR_HEIGHT will be significantly larger than specified on retina displays.
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	// Check if the left mouse button is pressed
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		float xpos = static_cast<float>(xposIn);
+		float ypos = static_cast<float>(yposIn);
+
+		if (mouseButtonPressed) {
+			lastX = xpos;
+			lastY = ypos;
+			mouseButtonPressed = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+		lastX = xpos;
+		lastY = ypos;
+
+		camera.ProcessMouseMovement(xoffset, yoffset);
+	}
+	else
+		mouseButtonPressed = true;
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void ProcessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// Utility function for loading a 2D texture from file
+unsigned int LoadTexture(const std::string& path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+	if (data) {
+		GLenum format = GL_RGBA;
+		if (nrComponents == 1) format = GL_RED;
+		else if (nrComponents == 3) format = GL_RGB;
+		else if (nrComponents == 4) format = GL_RGBA;
+		else {
+			std::cerr << "Invalid texture format: Unsupported number of components!\n";
+			return -1;
+		}
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		stbi_image_free(data);
+	}
+	else {
+		std::cerr << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+		return -1;
+	}
+
+	return textureID;
+}
