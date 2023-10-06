472bbdf98a0de25d63ba5df191ad27f19ae73514
余振寰 <1932878552@qq.com>
2023-10-06 22:49:29 +0100

    1

--- res/shaders/ssao.fs
+++ res/shaders/ssao.fs
@@ -18,14 +18,38 @@ const vec2 noiseScale = vec2(1920.0f/4.0f, 1080.0f/4.0f);
 
 void main()
 {
+    // Retrieve Position and Normal from G-Buffer, noise vector from noiseTexture
+    // -Sample the gPositionDepth and gNormal textures to get the fragment's position and normal in world space.
+    vec3 FragPos = texture(gPositionDepth, TexCoords).rgb;
+    vec3 Normal = texture(gNormal, TexCoords).rgb;
+    vec3 randomVec = texture(noiseTexture, TexCoords * noiseScale).rgb;
 
+    // Create TBN change-of-basis matrix: from tangent-space to view-space
+    vec3 tangent = normalize(randomVec - normal * dot(randomVec, normal));
+    vec3 bitangent = cross(normal, tangent);
+    mat3 TBN = mat3(tangent, bitangent, normal);
+
+    // SSAO Kernel Loop
+    float occlusion = 0.0f;
+    for(int i = 0; i < kernelSize; i++) {
+        // Sample Position
+        vec3 sample = TBN * samples[i];
+        sample = FragPos + sample * radius;
+
+        // project sample position (to sample texture)
+        vec4 offset = vec4(sample, 1.0f);
+        offset = projection * offset; // from view to clip-space
+        offset.xyz /= offset.w; // perspective divide
+        offset.xyz = offset.xyz * 0.5f + 0.5; // transform to range 0.0 - 1.0
+
+        // get sample depth
+        float sampleDepth = -texture(gPositionDepth, offset.xy).w; // Get depth value of kernel sample
+
+
+    }
 }
 
-// Retrieve Position and Normal from G-Buffer
-// -Sample the gPositionDepth and gNormal textures to get the fragment's position and normal in world space.
 
-// Retrieve Noise Vector
-// -Sample the noiseTexture using TexCoords scaled by noiseScale to get a random rotation vector.
 
 // Initialize Variables
 // -Initialize a variable to accumulate the occlusion factor. Let's call it occlusion.
